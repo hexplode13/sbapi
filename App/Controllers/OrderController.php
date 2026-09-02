6546fd0507365d8ea2aebd8ee6a3fa3fb1467b7b
hexplode13 <heplode13@google.com>
2026-09-02 17:24:41 +0300

    closeontab

--- App/Controllers/OrderController.php
+++ App/Controllers/OrderController.php
@@ -96,7 +96,7 @@ class OrderController extends Controller
         $orderId = (int)$params['id'];
 
         try {
-            $this->orders->closeontab($orderId, true);
+            $this->orders->closeontab($orderId, true); 
 
             $this->success([
                 'order' => $orderId,
